69260a4859cd0d8e2d2effda5d55a10e19e399af
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-23 11:31:06 +0100

    chore: add relevant  header icons

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View, TouchableOpacity } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import ProductItem from '../../components/shop/ProductItem';
-import * as cartActions from '../../store/actions/cart'
+
+//  icons
+import { Feather } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+
+import * as cartActions from '../../store/actions/cart';
 
 const ProductOverviewScreen = (props) => {
   const { availableProducts: products } = useSelector(
     (state) => state.products
   );
-  const dispatch = useDispatch()
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    props.navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          style={{ paddingLeft: 15 }}
+          onPress={() => props.navigation.toggleDrawer()}
+        >
+          <Feather name="menu" size={24} color="black" />
+        </TouchableOpacity>
+      ),
+
+      headerRight: () => (
+        <View style={{ paddingRight: 15 }}>
+          <Entypo
+            name="shopping-cart"
+            size={24}
+            color="black"
+            onPress={() => props.navigation.navigate('Cart Screen')}
+          />
+        </View>
+      ),
+    });
+  });
 
   return (
     <FlatList
@@ -26,13 +56,12 @@ const ProductOverviewScreen = (props) => {
             })
           }
           onAddToCart={() => {
-             dispatch(cartActions.addToCart(itemData.item)) 
-             props.navigation.navigate('Cart Screen', {
+            dispatch(cartActions.addToCart(itemData.item));
+            props.navigation.navigate('Cart Screen', {
               productId: itemData.item.id,
               title: itemData.item.title,
-            })
-             
-             }}
+            });
+          }}
         />
       )}
     />

fb32221c21d7ebd0ece09c142c729d4417327642
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-23 11:29:55 +0100

    chore: use actual icons

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -7,6 +7,7 @@ import {
   Platform,
   Button,
 } from 'react-native';
+
 import { Ionicons } from '@expo/vector-icons';
 
 
@@ -20,13 +21,18 @@ const CartItem = (props) => {
       </View>
       <View style={styles.itemData}>
         <Text style={styles.mainText}>{props.amount.toFixed(2)}</Text>
-        {Platform.OS === 'android' ? <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
+        {/* {Platform.OS === 'android' ? <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
+          <Ionicons name="ios-trash" size={24} color="black" />
+        </TouchableOpacity> : <Button title="Remove" color="red" onPress={props.onRemove}/>} */}
           <Ionicons
-            name={Platform.OS === 'android' ? 'md-trash' : 'md-trash'}
+            name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
             size={23}
             color="red"
+            onPress={props.onRemove}
+            style={{
+              marginLeft: 8
+            }}
           />
-        </TouchableOpacity> : <Button title="Remove" color="red" onPress={props.onRemove}/>}
       </View>
     </View>
   );
@@ -45,6 +51,7 @@ const styles = StyleSheet.create({
   itemData: {
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: 20
   },
   quantity: {
     color: '#888',
